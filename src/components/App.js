fd6feb9b426dfa217ead4a4493bbe95266423464
Yosuke <yousuke@ageet.com>
2022-02-13 15:32:12 +0900

    save name in japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import SignIn from './SignIn'
 import {useState} from 'react'
+import Main from './main'
 
 function App() {
   const [name,setName] = useState("")
   console.log({name})
-  return (
-    <div className="App">
-      <SignIn setName={setName}/>
-    </div>
-  );
+  if (name == '') {
+    return (
+      <div className="App">
+        <SignIn setName={setName}/>
+      </div>
+    );
+  } else {
+    return (
+      <Main name={name}/>
+    )
+  }
 }
 
 export default App;
